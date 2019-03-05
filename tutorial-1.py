57832a89200de7bfb22216d1150c1eb2fa88b560
kemfic <19ficici.kemal@gmail.com>
2019-03-04 21:24:17 -0500

    playing around with opengl

--- tutorial-1.py
+++ tutorial-1.py
@@ -1,2 +1,38 @@
-import OpenGL as gl
+# https://pythonprogramming.net/opengl-rotating-cube-example-pyopengl-tutorial/
+
 import pygame as pg
+
+import OpenGL.gl as gl
+import OpenGL.GLU as glu
+
+import numpy as np
+
+vertices =(
+        (1, -1, -1),
+        (1, 1, -1),
+        (-1, 1, -1),
+        (-1, -1, -1),
+        (1, -1, 1),
+        (1, 1, 1),
+        (-1, -1, 1),
+        (-1, 1, 1)
+        )
+
+edges = (
+        (0,1),
+        (0,3),
+        (0,4),
+        (2,1),
+        (2,3),
+        (2,7),
+        (6,3),
+        (6,4),
+        (6,7),
+        (5,1),
+        (5,4),
+        (5,7)
+        )
+
+
+def Cube():
+  gl.glBegin
