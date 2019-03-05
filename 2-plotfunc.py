d4789e7acc605e8e51741ddf36d6dc4eb167f43b
kemfic <19ficici.kemal@gmail.com>
2019-03-04 23:27:08 -0500

    finished function plotter

--- 2-plotfunc.py
+++ 2-plotfunc.py
@@ -7,30 +7,30 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
+import numpy as np
 
 
+dom = 3.0 #domain
 
 def init():
   
-  glClearColor(0.0, 0.0, 0.0, 1.0) # (r, g, b, a) tells OpenGL what color we use to clear
-  gluOrtho2D(-1.0, 1.0, -1.0, 1.0) # sets coord system ranges (x_min, x_max, y_min, y_max)
+  glClearColor(0.2, 0.2, 0.2, 1.0) # (r, g, b, a) tells OpenGL what color we use to clear
+  gluOrtho2D(-dom, dom, -dom, dom) # sets coord system ranges (x_min, x_max, y_min, y_max)
   # center is 0,0 
 
-def plotpoints():
+def plotfunc():
   # clears color buffer bit
   glClear(GL_COLOR_BUFFER_BIT)
   # sets next color
-  glColor3f(1.0, 0.0, 0.0)
+  glColor3f(1.0, 0.0, 1.0)
   
   # draws points
-  glPointSize(10.0)
+  glPointSize(3.0)
   glBegin(GL_POINTS)
-  glVertex2f(0.0, 0.0)
-  glVertex2f(0.5, 0.5)
-  glVertex2f(-0.5, 0.5)
-  glVertex2f(0.5, -0.5)
-  glVertex2f(-0.5, -0.5)
 
+  for x in np.arange(-dom, dom, dom/50.0):
+    y=np.tanh(x)
+    glVertex2f(x,y)
   glEnd()
   glFlush()
 
@@ -40,6 +40,6 @@ if __name__ == "__main__":
   glutInitWindowSize(500, 500)
   glutInitWindowPosition(1080,100)
   glutCreateWindow("Plot Points")
-  glutDisplayFunc(plotpoints)
+  glutDisplayFunc(plotfunc)
   init()
   glutMainLoop()
