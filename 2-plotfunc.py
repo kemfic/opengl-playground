47efa8bdca39e124a744a9fd4cfd03be9100a615
kemfic <19ficici.kemal@gmail.com>
2019-03-04 23:28:31 -0500

    finished function plotter

--- 2-plotfunc.py
+++ 2-plotfunc.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
   glutInitWindowSize(500, 500)
   glutInitWindowPosition(1080,100)
-  glutCreateWindow("Plot Points")
+  glutCreateWindow("Plot Function")
   glutDisplayFunc(plotfunc)
   init()
   glutMainLoop()
