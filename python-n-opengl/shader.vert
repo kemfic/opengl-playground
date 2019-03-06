afd35584d5cb6b9eb6bb737a99022880f02770bb
kemfic <19ficici.kemal@gmail.com>
2019-03-06 17:46:04 -0500

    removed stuff

--- python-n-opengl/shader.vert
+++
@@ -1,6 +0,0 @@
-attribute vec2 position;
-attribute vec3 color;
-
-void main() {
-  gl_Position = vec3(position, 0.0, 1.0);
-}
--- python-n-opengl/vertex_structure.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-data = np.zeros(4, dtype= [ ("position", np.float32, 2),
-                            ("color", np.float32, 3)] )
-
