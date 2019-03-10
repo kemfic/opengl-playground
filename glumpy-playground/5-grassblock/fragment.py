d5b4dea2c0917c0cbe320ba26dd9a17ee9f97197
kemfic <19ficici.kemal@gmail.com>
2019-03-09 23:38:56 -0500

    removed outline, texture works for a single face

--- glumpy-playground/5-grassblock/fragment.py
+++ glumpy-playground/5-grassblock/fragment.py
@@ -1,4 +1,5 @@
 fragment = """
+uniform float alpha_thresh = 0.1;
 uniform vec4      u_color;    // Global color
 uniform sampler2D u_texture;  // Texture 
 varying vec4      v_color;    // Interpolated fragment color (in)
@@ -7,5 +8,8 @@ void main()
 {
     // Get texture color
     gl_FragColor = texture2D(u_texture, v_texcoord);
+    if (gl_FragColor.a < alpha_thresh){
+    discard;
+    }
 }
 """
