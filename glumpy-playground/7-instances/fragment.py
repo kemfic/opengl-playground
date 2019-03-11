8838a908cbef82e4b7010ca20c2126440dae494e
kemfic <19ficici.kemal@gmail.com>
2019-03-11 02:20:49 -0400

    final commit tonight

---
+++ glumpy-playground/7-instances/fragment.py
@@ -0,0 +1,25 @@
+"""
+Texture blending in GLSL
+
+http://distrustsimplicity.net/articles/texture-blending-in-glsl/
+"""
+
+fragment = """
+uniform float alpha_thresh = 0.1;
+uniform vec4      u_color;    // Global color
+uniform sampler2D u_texture;  // Texture 
+varying vec4      v_color;    // Interpolated fragment color (in)
+varying vec2      v_texcoord0; // Interpolated fragment texture coordinates (in)
+varying vec2      v_texcoord1; // used for stacking textures
+void main()
+{
+    // Get texture color
+    vec4 t0 = texture2D(u_texture, v_texcoord0);
+    vec4 t1 = texture2D(u_texture, v_texcoord1);
+
+    gl_FragColor = mix(t0, t1, t1.a);
+    if (gl_FragColor.a < alpha_thresh){
+    discard;
+    }
+}
+"""
